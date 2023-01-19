39cb9e55811a67e9a90ca64ef6f914a4c28fc1c6
Oleh Savaryn <olegsavaryn@gmail.com>
2023-01-19 17:25:22 +0100

    Update App.jsx

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,16 +1,30 @@
 import { Routes, Route } from 'react-router-dom';
+import { lazy, Suspense } from 'react';
 import NotFound from './404Page/NotFound';
-import Cast from '../components/Cast/Cast';
-import MovieDetails from '../components/MovieDetails/MovieDetails';
-import Header from './Header/Header';
-import Movies from './Movies/Movies';
-import Review from '../components/Review/Review';
-import TrendingFilms from '../components/TrendingFilms/TrendingFilms';
+// import Cast from '../components/Cast/Cast';
+// import MovieDetails from '../components/MovieDetails/MovieDetails';
+// import Header from './Header/Header';
+// import Movies from './Movies/Movies';
+// import Review from '../components/Review/Review';
+//  import TrendingFilms from '../components/TrendingFilms/TrendingFilms';
+
+const TrendingFilms = lazy(() =>
+  import('../components/TrendingFilms/TrendingFilms')
+);
+
+const Review = lazy(() => import('../components/Review/Review'));
+
+const Movies = lazy(() => import('./Movies/Movies'));
+const Header = lazy(() => import('./Header/Header'));
+const MovieDetails = lazy(() =>
+  import('../components/MovieDetails/MovieDetails')
+);
+const Cast = lazy(() => import('../components/Cast/Cast'));
 
 export const App = () => {
   return (
     <>
-      <div>
+      <Suspense fallback={<div>Loading...</div>}>
         <Routes>
           <Route path="/" element={<Header />}>
             <Route index element={<TrendingFilms />} />
@@ -22,7 +36,7 @@ export const App = () => {
           </Route>
           <Route path="*" element={<NotFound />} />
         </Routes>
-      </div>
+      </Suspense>
     </>
   );
 };
